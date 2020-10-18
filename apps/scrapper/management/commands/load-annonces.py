72e9b5c7b51a651926dd5a379f8f6e811ec618dd
Geoffroy Blondel <geoffroy.blondel@gmail.com>
2020-10-18 12:07:00 +0200

    create admin command to clean log directory

---
+++ apps/scrapper/management/commands/load-annonces.py
@@ -0,0 +1,33 @@
+from django.core.management.base import BaseCommand, CommandError
+from apps.scrapper.scrapper import AnnonceListScrapper
+#  Draft command line
+#  Telecharger les données pour toute les annonces
+#  > load-data --all
+#  > load-data --url  --all
+#  > load-data --url  --last
+
+
+class Command(BaseCommand):
+    help='From a search URL, load all add urls'
+
+    def add_arguments(self, parser):
+        parser.add_argument('url', nargs='+', type=str)
+        parser.add_argument(
+           '--all', 
+           action='store_true', 
+           help='charge toute les données',
+        )        
+        parser.add_argument(
+           '--last', 
+           action='store_true', 
+           help='charge toute les données',
+        )
+
+
+    def handle(self, *args, **options):
+        if options['all']:
+            print(options['url'][0])
+            AnnonceListScrapper(options['url'][0])
+        if options['last']:
+            print('last')
+        
